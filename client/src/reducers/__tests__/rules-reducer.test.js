20f88a898aa496dc0e156a2f78bbf1dbb24b39ad
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-29 12:45:25 +0800

    04-06:Likes and Dislikes Reducer

--- client/src/reducers/__tests__/rules-reducer.test.js
+++ client/src/reducers/__tests__/rules-reducer.test.js
@@ -2,6 +2,7 @@ import deepFreeze from "deep-freeze";
 import rules from "../../data.json";
 import reducer from "../rules-reducer";
 import { RULES_LOADED } from "../../actions/rules-actions";
+import { DO_LIKE, DO_DISLIKE } from "../../actions/likes-actions";
 
 describe("Rules reducer", () => {
   test("should return the initial state", () => {
@@ -27,5 +28,33 @@ describe("Rules reducer", () => {
     const newState = reducer(previousState, action);
 
     expect(newState).toEqual(expectedNewState);
+  });
+
+  test("should increment likes", () => {
+    const action = {
+      type: DO_LIKE,
+      payload: 1
+    };
+
+    const previousState = rules;
+
+    deepFreeze(previousState);
+    const newState = reducer(previousState, action);
+
+    expect(newState[0].likes).toBe(1);
+  });
+
+  test("should increment dislikes", () => {
+    const action = {
+      type: DO_DISLIKE,
+      payload: 1
+    };
+
+    const previousState = rules;
+
+    deepFreeze(previousState);
+    const newState = reducer(previousState, action);
+
+    expect(newState[0].dislikes).toBe(1);
   });
 });
