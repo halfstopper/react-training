f1dc8bf478500ea2ee9e7bff7f39af97368a03af
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-29 17:45:37 +0800

    07-01:Form binding

--- client/src/RuleForm.js
+++ client/src/RuleForm.js
@@ -1,23 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Formik, Form, Field } from "formik";
 import RuleTitleField from "./RuleTitleField";
 import RuleDescriptionField from "./RuleDescriptionField";
 
 const RuleForm = ({ rule: { id, title, description } }) => {
+  const initialValues = {
+    id,
+    title: title || "",
+    description: description || ""
+  };
   return (
     <div className="panel panel-primary">
       <div className="panel-heading">
         <h3 className="panel-title"> {id ? "Edit rule" : "New rule"} </h3>
       </div>
       <div className="panel-body">
-        <form>
-          <RuleTitleField title={title} />
-          <RuleDescriptionField description={description} />
-          <button type="submit" className="btn btn-primary pull-right">
-            Submit
-          </button>
-        </form>
+        <Formik
+          onSubmit={values => console.log(values)}
+          initialValues={initialValues}
+          render={({ errors, dirty, isSubmitting }) => (
+            <Form>
+              <Field name="title" component={RuleTitleField} />
+              <Field name="description" component={RuleDescriptionField} />
+              <button type="submit" className="btn btn-primary pull-right">
+                Submit
+              </button>
+            </Form>
+          )}
+        />
       </div>
     </div>
   );
